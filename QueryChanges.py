1e63e40cdcd6378cb36a43d18f8f7792123594e5
spatialpete <shermanps@gmail.com>
2018-01-10 18:35:30 -0500

    added list comparison to new and old query

--- QueryChanges.py
+++ QueryChanges.py
@@ -11,7 +11,7 @@ res = requests.get('http://google.com/search?q=' + queryTerm)
 res.raise_for_status()
 
 # return results as text
-soup = bs4.BeautifulSoup(res.text)
+soup = bs4.BeautifulSoup(res.text, "html.parser")
 
 # select results
 linkElems = soup.select('.r a')
@@ -19,15 +19,28 @@ print(linkElems[1])
 print(len(linkElems))
 
 # write results to text file
-outFile = open('E:/Python/SearchResults.txt', 'w')
-for i in linkElems:
-    outFile.write(str(i) + '\n')
-outFile.close()
+#outFile = open('E:/Python/SearchResults.txt', 'w')
+#for i in linkElems:
+    #outFile.write(str(i) + '\n')
+#outFile.close()
 
 # todo: if outFile already exists, compare new file to old file
 # this will need to be moved above the write results to text file part
+compareList = []
 if os.path.exists('E:/Python/SearchResults.txt'):
-    oldFileList = outFile.readlines()
-    print oldFileList
+    oldFile = open('E:/Python/SearchResults.txt', 'r')
+    oldFileList = oldFile.readlines()
+    print(oldFileList[1])
+    for newItem in linkElems:
+        #for oldItem in oldFileList:
+        if newItem in oldFileList:
+            compareList.append(newItem)
+            print('new search result found')
+            print("1: " + str(oldItem) + '\n 2: ' + str(newItem))
+else:
+    outFile = open('E:/Python/SearchResults.txt', 'w')
+    for i in linkElems:
+        outFile.write(str(i) + '\n')
+    outFile.close()
 
 # todo: email any changes
